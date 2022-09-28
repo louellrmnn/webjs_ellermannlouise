b88cca0e32a1706c78ef3666b6a1b65a90424ebb
Louise Ellermann <luise@MacBook-Pro-de-Louise.local>
2022-09-28 16:43:11 +0200

    webjs

--- script.js
+++ script.js
@@ -14,3 +14,50 @@ var count = 0;
     count++;
     disp.innerHTML = count;
     }
+
+localStorage.setItem ("prenom", "louise");
+localStorage.setItem ("nom", "ellermann");
+
+localStorage.getItem ("prenom");
+localStorage.getItem ("nom");
+
+
+
+let objCar1 = {
+	marque : "Audi",
+	couleur : "Noire",
+}
+let objLinea1 = JSON.stringify(objCar1);
+
+let objCar2 = {
+	marque : "Renault",
+	couleur : "Grise",
+}
+let objLinea2 = JSON.stringify(objCar2);
+
+
+let objCar3 = {
+	marque : "Mini",
+	couleur : "Rouge",
+}
+let objLinea3 = JSON.stringify(objCar3);
+
+let objCar4 = {
+	marque : "Fiat",
+	couleur : "Bleue",
+}
+let objLinea4 = JSON.stringify(objCar4);
+
+let objCar5 = {
+	marque : "Nissan",
+	couleur : "Rose",
+}
+let objLinea5 = JSON.stringify(objCar5);
+
+localStorage.setItem("obj1", objLinea1);
+localStorage.setItem("obj2", objLinea2);
+localStorage.setItem("obj3", objLinea3);
+localStorage.setItem("obj4", objLinea4);
+localStorage.setItem("obj5", objLinea5);
+
+
