85c496d12e84d5cef9c20d0fde93cee7e361354d
Louise Ellermann <luise@MacBook-Pro-de-Louise.local>
2022-09-28 15:47:11 +0200

    webjs

--- script.js
+++ script.js
@@ -5,3 +5,12 @@ const newTab = document.createElement("div");
 let tab = document.getElementById("conteneur");
 
 tableau.appendChild(newTab);
+
+var count = 0;
+    var boutton = document.getElementById("cliquer");
+    var disp = document.getElementById("display");
+  
+    boutton.onclick = function () {
+    count++;
+    disp.innerHTML = count;
+    }
